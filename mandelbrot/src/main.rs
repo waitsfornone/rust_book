34062ce05b892a90e03890cc2c576dc1669fd821
Tim Enders <enders.tim@gmail.com>
2022-01-05 16:26:27 -0600

    working now

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -38,7 +38,7 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..0])) {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l, r)),
                 _ => None
             }
